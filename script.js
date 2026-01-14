ef6301b65d0017aa0d84fc0cab01c49d24001726
elltd <elliotcdong@gmail.com>
2026-01-13 19:43:16 -0500

    change 13

--- script.js
+++ script.js
@@ -15,6 +15,24 @@ function changeRole(){
     }
 }
 
+//role change automatic
+function displayWord() {
+	var words = document.getElementsByClassName("toggle");
+  var wordCounter = 0;
+  
+  setInterval(updateWord,3000);
+  
+  function updateWord() {
+  	if (wordCounter >= words.length) wordCounter = 0;
+  	for(var i = 0; i < words.length; i++) {
+    	words[i].classList.remove('active');
+    }
+		words[wordCounter].classList.add('active');
+   	wordCounter++;
+  }
+}
+displayWord();
+
 
 //tooltip follow cursor
 document.addEventListener('DOMContentLoaded', function(){
